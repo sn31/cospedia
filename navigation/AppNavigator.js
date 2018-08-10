018d9baee1d3781c0a73cc8c99c1c9a16adde262
Hyewon Cho <jhng2525@gmail.com>
2018-08-09 22:23:43 -0700

    Fixed deprecated TabNavigator issue.

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { TabNavigator, TabBarBottom } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation';
 import Colors from '../constants/Colors';
 import MainTabNavigator from './MainTabNavigator';
 import HomeScreen from '../screens/HomeScreen';
 
-export default TabNavigator(
+export default createBottomTabNavigator(
   {
     Test: {
       screen: HomeScreen,
@@ -25,14 +25,13 @@ export default TabNavigator(
                 : 'md-information-circle';
             break;
         }
-        return (
+        return
           <Ionicons
             name={iconName}
             size={28}
             style={{ marginBottom: -3 }}
             color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
-          />
-        );
+          />;
       },
     }),
     // tabBarComponent: TabBarBottom,
